84cd26ff798884f70fcfabdb8ad2465cd3c90e10
Ayohbk <dejihbk012@gmail.com>
2021-11-26 14:33:54 +0100

    refactor

--- test/specs/loginpage.test.js
+++ test/specs/loginpage.test.js
@@ -1,7 +1,7 @@
 const dialog = require("../pageobjects/dialog.page.js");
 
 describe("My Login page", () => {
-  it("should notify user of invalid email address entered", async () => {
+  before(async () => {
     //Selenium opens browser
     await dialog.open();
     //This set additional time wait for the cookie consent page to load
@@ -12,10 +12,13 @@ describe("My Login page", () => {
     await expect(dialog.loginPageTitle).toHaveText("Login to simpleshow");
     //User types invalid email
     await dialog.emailAddressField.setValue("TE5T123");
-    //
+    //User moves the cursor to the password field
     await dialog.emailAddressField.setValue("Tab");
-    //await driver.keys("Enter");
+  });
+  it("should notify user of invalid email address entered", async () => {
     await expect(dialog.errorMessageField).toHaveText("Invalid e-mail address");
+  });
+  it("Login button should be disabled", async () => {
     expect(dialog.loginBtn).toBeDisabled;
   });
 });
